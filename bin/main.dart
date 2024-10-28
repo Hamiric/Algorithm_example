5ffdd8a56ee464af32bcede2735c437c12a24c32
Hamiric <do9870@naver.com>
2024-10-28 19:59:09 +0900

    Fix : "28문제 함수코드 이름 수정"
    28번 문제 함수코드 이름 수정

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,10 @@
 import 'package:algorithm_ex/findthedifference_389.dart';
-import 'package:algorithm_ex/strStr_28.dart';
+import 'package:algorithm_ex/str_str_28.dart';
 
 void main() {
   // 389번 문제
-  print(findTheDifference('sadb','sagdb'));
+  print(findTheDifference('sadb','sagdb'));  // g
 
   // 28번 문제
-  print(strStr('leetcode', 'sad'));
+  print(strStr('leetcode', 'sad'));  // -1
 }
